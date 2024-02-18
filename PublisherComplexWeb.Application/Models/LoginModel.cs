5980acb60075f7138dc4c8bc57e33b14709206bb
Hihiz <public@mail.ru>
2024-02-18 17:53:39 +0300

    Добавить модели для прохождения аутентификации, регистрации

---
+++ PublisherComplexWeb.Application/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PublisherComplexWeb.Application.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
