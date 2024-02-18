4c0ee8985c9e460f78ab65aeae4fdca1b151f3cc
Hihiz <public@mail.ru>
2024-02-18 17:42:26 +0300

    Добавить базовые репозитории

---
+++ PublisherComplexWeb.Application/Interfaces/IBaseRepositoryWorkOrder.cs
@@ -0,0 +1,12 @@
+﻿namespace PublisherComplexWeb.Application.Interfaces
+{
+    public interface IBaseRepositoryWorkOrder<T>
+    {
+        /// <summary>
+        /// Получить все работы к заказу
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        Task<List<T>> GetWorksOrder(int orderId);
+    }
+}
