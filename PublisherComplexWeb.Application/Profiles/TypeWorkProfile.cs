0907413ad0f01968dad7846188c7346af20bb363
Hihiz <public@mail.ru>
2024-02-19 22:12:44 +0300

    Создать профили для автомаппера

---
+++ PublisherComplexWeb.Application/Profiles/TypeWorkProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using PublisherComplexWeb.Application.Dto.TypeWork;
+using PublisherComplexWeb.Domain.Entities;
+
+namespace PublisherComplexWeb.Application.Profiles
+{
+    public class TypeWorkProfile : Profile
+    {
+        public TypeWorkProfile()
+        {
+            CreateMap<TypeWork, TypeWorkDto>().ReverseMap();
+            CreateMap<TypeWork, CreateTypeWorkDto>().ReverseMap();
+            CreateMap<TypeWork, UpdateTypeWorkDto>().ReverseMap();
+        }
+    }
+}
