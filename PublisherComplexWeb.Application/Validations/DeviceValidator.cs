c9ead8abe03361576a2cf19d78dda27e3916b710
Hihiz <public@mail.ru>
2024-02-20 15:26:16 +0300

    Имплементировать валидацию

---
+++ PublisherComplexWeb.Application/Validations/DeviceValidator.cs
@@ -0,0 +1,34 @@
+﻿using PublisherComplexWeb.Application.Interfaces;
+using PublisherComplexWeb.Application.Models;
+using PublisherComplexWeb.Domain.Entities;
+using PublisherComplexWeb.Domain.Enums;
+
+namespace PublisherComplexWeb.Application.Validations
+{
+    public class DeviceValidator : IValidator<Device>
+    {
+        private readonly IBaseRepository<Device> _repository;
+
+        public DeviceValidator(IBaseRepository<Device> repository) => _repository = repository;
+
+        public async Task<IBaseStatus> Validate(Device entity)
+        {
+            Device result = (await _repository.GetAll()).FirstOrDefault(d => d.Title == entity.Title);
+
+            if (result != null)
+            {
+                return new StatusResponse()
+                {
+                    StatusCode = (int)ErrorCode.AlreadyExists,
+                    Message = ErrorCode.AlreadyExists.ToString()
+                };
+            }
+
+            return new StatusResponse()
+            {
+                StatusCode = (int)ErrorCode.OK,
+                Message = ErrorCode.OK.ToString()
+            };
+        }
+    }
+}
---
+++ PublisherComplexWeb.Application/Validations/TypeWorkValidator.cs
@@ -0,0 +1,34 @@
+﻿using PublisherComplexWeb.Application.Interfaces;
+using PublisherComplexWeb.Application.Models;
+using PublisherComplexWeb.Domain.Entities;
+using PublisherComplexWeb.Domain.Enums;
+
+namespace PublisherComplexWeb.Application.Validations
+{
+    public class TypeWorkValidator : IValidator<TypeWork>
+    {
+        private readonly IBaseRepository<TypeWork> _repository;
+
+        public TypeWorkValidator(IBaseRepository<TypeWork> repository) => _repository = repository;
+
+        public async Task<IBaseStatus> Validate(TypeWork entity)
+        {
+            TypeWork result = (await _repository.GetAll()).FirstOrDefault(t => t.Title == entity.Title);
+
+            if (result != null)
+            {
+                return new StatusResponse()
+                {
+                    StatusCode = (int)ErrorCode.AlreadyExists,
+                    Message = ErrorCode.AlreadyExists.ToString()
+                };
+            }
+
+            return new StatusResponse()
+            {
+                StatusCode = (int)ErrorCode.OK,
+                Message = ErrorCode.OK.ToString()
+            };
+        }
+    }
+}
