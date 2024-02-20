da5c55e501301f7759c25dce102f42798a863720
Hihiz <public@mail.ru>
2024-02-20 15:24:53 +0300

    Создать интерфейс для валидации

---
+++ PublisherComplexWeb.Application/Validations/IValidator.cs
@@ -0,0 +1,9 @@
+﻿using PublisherComplexWeb.Application.Interfaces;
+
+namespace PublisherComplexWeb.Application.Validations
+{
+    public interface IValidator<T>
+    {
+        Task<IBaseStatus> Validate(T entity);
+    }
+}
