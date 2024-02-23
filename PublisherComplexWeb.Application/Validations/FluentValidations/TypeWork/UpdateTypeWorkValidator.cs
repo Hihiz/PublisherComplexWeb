2e33afa68aba1b79915a55b9bb7d0682eefdff2f
Hihiz <public@mail.ru>
2024-02-23 21:14:30 +0300

    Добавить валидацию FluentValidation к моделям дто

---
+++ PublisherComplexWeb.Application/Validations/FluentValidations/TypeWork/UpdateTypeWorkValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using PublisherComplexWeb.Application.Dto.TypeWork;
+
+namespace PublisherComplexWeb.Application.Validations.FluentValidations.TypeWork
+{
+    public class UpdateTypeWorkValidator : AbstractValidator<UpdateTypeWorkDto>
+    {
+        public UpdateTypeWorkValidator()
+        {
+            RuleFor(x => x.Title)
+                    .NotEmpty().WithMessage("Введите тип работы")
+                    .MinimumLength(3).WithMessage("Название типа работы короткое");
+        }
+    }
+}
