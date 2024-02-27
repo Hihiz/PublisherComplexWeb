3f8d0a8bb1b75273c04ee75dfad485e8896d4ace
Hihiz <public@mail.ru>
2024-02-27 23:55:33 +0300

    Имплементировать репозиторий для типов работ

---
+++ PublisherComplexWeb.Infrastructure/Repositories/TypeWorkRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherComplexWeb.Application.Interfaces;
+using PublisherComplexWeb.Domain.Entities;
+using PublisherComplexWeb.Infrastructure.Data;
+
+namespace PublisherComplexWeb.Infrastructure.Repositories
+{
+    public class TypeWorkRepository : IBaseRepository<TypeWork>
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TypeWorkRepository(ApplicationDbContext db) => _db = db;
+
+        public async Task<List<TypeWork>> GetAll() => await _db.TypeWorks.OrderBy(t => t.Id).Include(t => t.Works).AsNoTracking().ToListAsync();
+
+        public async Task<TypeWork> GetById(int id) => await _db.TypeWorks.Include(t => t.Works).AsNoTracking().FirstOrDefaultAsync(t => t.Id == id);
+
+        public async Task<TypeWork> Create(TypeWork entity)
+        {
+            await _db.TypeWorks.AddAsync(entity);
+
+            return entity;
+        }
+
+        public async Task<TypeWork> Update(TypeWork entity)
+        {
+            _db.TypeWorks.Update(entity);
+
+            return entity;
+        }
+
+        public async Task Delete(TypeWork entity) => _db.TypeWorks.Remove(entity);
+
+        public async Task SaveChangesAsync() => await _db.SaveChangesAsync();
+    }
+}
