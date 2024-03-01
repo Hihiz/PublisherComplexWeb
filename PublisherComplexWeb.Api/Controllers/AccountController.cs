c8286a17123dc3181d7ac4b05202c7ba88247d2a
Hihiz <public@mail.ru>
2024-03-01 23:53:46 +0300

    Создать контроллер для прохождения авторизации

---
+++ PublisherComplexWeb.Api/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PublisherComplexWeb.Application.Dto.User;
+using PublisherComplexWeb.Application.Interfaces;
+using PublisherComplexWeb.Infrastructure.Interfaces;
+using PublisherComplexWeb.Infrastructure.Models;
+
+namespace PublisherComplexWeb.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IUserAuthenticationService _authService;
+        private readonly IUserAuthService<UserDto> _userService;
+
+        public AccountController(IUserAuthenticationService authService, IUserAuthService<UserDto> userService) => (_authService, _userService) = (authService, userService);
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegistrationModel registration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Error");
+            }
+
+            return Ok(await _authService.RegisterAsync(registration));
+        }
+    }
+}
