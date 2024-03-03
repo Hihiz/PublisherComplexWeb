c4264546a9736613f30f9f80db8aa6f2774bffbb
Hihiz <public@mail.ru>
2024-03-03 17:40:49 +0300

    Создать контроллер работ

---
+++ PublisherComplexWeb.Api/Controllers/WorkController.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using PublisherComplexWeb.Application.Dto.Work;
+using PublisherComplexWeb.Application.Interfaces;
+using PublisherComplexWeb.Application.Validations.FluentValidations.Work;
+
+namespace PublisherComplexWeb.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkController : ControllerBase
+    {
+        private readonly IWorkService _workService;
+
+        public WorkController(IWorkService workService) => (_workService) = (workService);
+
+        [HttpGet]
+        public async Task<ActionResult> GetWorks() => Ok(await _workService.GetAll());
+
+        [HttpGet("workDetail/{id}")]
+        public async Task<ActionResult> GetWork(int id) => Ok(await _workService.GetById(id));
+
+        [HttpGet("{orderId}")]
+        public async Task<ActionResult> GetWorksOrder(int orderId) => Ok(await _workService.GetByWorksOrder(orderId));
+
+        [HttpPost]
+        public async Task<ActionResult> CreateWork(CreateWorkDto dto)
+        {
+            CreateWorkValidator validators = new CreateWorkValidator();
+
+            ValidationResult result = validators.Validate(dto);
+
+            if (!result.IsValid)
+            {
+                return BadRequest(string.Join('\n', result.Errors));
+            }
+
+            return Ok(await _workService.CreateWork(dto));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateWork(int id, UpdateWorkDto dto)
+        {
+            UpdateWorkValidator validators = new UpdateWorkValidator();
+
+            ValidationResult result = validators.Validate(dto);
+
+            if (!result.IsValid)
+            {
+                return BadRequest(string.Join('\n', result.Errors));
+            }
+
+            return Ok(await _workService.UpdateWork(id, dto));
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> WorkDelete(int id)
+        {
+            await _workService.DeleteWork(id);
+
+            return Ok(id);
+        }
+    }
+}
