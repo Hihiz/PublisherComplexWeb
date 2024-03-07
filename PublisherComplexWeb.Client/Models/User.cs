0bb1021f7db128411453b89d851b1db7ee1a6778
Hihiz <public@mail.ru>
2024-03-08 02:59:12 +0300

    Изменить тип данных

--- PublisherComplexWeb.Client/Models/User.cs
+++ PublisherComplexWeb.Client/Models/User.cs
@@ -2,8 +2,8 @@
 {
     public class User
     {
-        public int Id { get; set; }
-        public string Email { get; set; }
+        public long Id { get; set; }
         public string Fio { get; set; }
+        public string Email { get; set; }
     }
 }
