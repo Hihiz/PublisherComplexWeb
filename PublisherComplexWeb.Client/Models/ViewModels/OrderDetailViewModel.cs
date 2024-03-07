71456b1051eb6aaec7e3adb67787b19239ec300f
Hihiz <public@mail.ru>
2024-03-08 02:55:40 +0300

    Добавить модели представления

---
+++ PublisherComplexWeb.Client/Models/ViewModels/OrderDetailViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PublisherComplexWeb.Client.Models.ViewModels
+{
+    public class OrderDetailViewModel
+    {
+        public Order Data { get; set; }
+
+        // Работы заказа
+        public List<Work> Work { get; set; }
+    }
+}
