7c0ca79102e2bfcb0396147a9e021f9762221a42
Hihiz <public@mail.ru>
2024-03-09 03:31:29 +0300

    Создать интерфейс для работы с рефреш токеном клиента

---
+++ PublisherComplexWeb.Client/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using PublisherComplexWeb.Client.Models;
+
+namespace PublisherComplexWeb.Client.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<TokenModel> RefreshToken(TokenModel token);
+    }
+}
