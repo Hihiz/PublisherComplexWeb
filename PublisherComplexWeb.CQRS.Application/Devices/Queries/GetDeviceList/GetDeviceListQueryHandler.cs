478c73c64e039e36905f116b2cba89fd4ed8192b
Hihiz <public@mail.ru>
2024-04-03 23:37:43 +0300

    Создать запрос получения списка устройств

---
+++ PublisherComplexWeb.CQRS.Application/Devices/Queries/GetDeviceList/GetDeviceListQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Devices.Queries.GetDeviceList
+{
+    public class GetDeviceListQueryHandler : IRequestHandler<GetDeviceListQuery, List<DeviceDto>>
+    {
+        private readonly IBaseRepository<Device> _repository;
+        private readonly IMapper _mapper;
+
+        public GetDeviceListQueryHandler(IBaseRepository<Device> repository, IMapper mapper) =>
+            (_repository, _mapper) = (repository, mapper);
+
+        public async Task<List<DeviceDto>> Handle(GetDeviceListQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+              
+            }
+            catch (Exception ex)
+            {
+               
+            }
+        }
+    }
+}
