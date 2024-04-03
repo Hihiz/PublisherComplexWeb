878815da360ad1ebccb48937860b5e3c4461977e
Hihiz <public@mail.ru>
2024-04-03 23:43:42 +0300

    Создать команду редактирования устройства

---
+++ PublisherComplexWeb.CQRS.Application/Devices/Commands/UpdateDevice/UpdateDeviceCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Devices.Commands.UpdateDevice
+{
+    public class UpdateDeviceCommandHandler : IRequestHandler<UpdateDeviceCommand>
+    {
+        private readonly IBaseRepository<Device> _repository;
+        private readonly IValidatorDto<Device> _validator;
+
+        public UpdateDeviceCommandHandler(IBaseRepository<Device> repository, IValidatorDto<Device> validator) =>
+            (_repository, _validator) = (repository, validator);
+
+        public async Task<UpdateDeviceDto> Handle(UpdateDeviceCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+            
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
