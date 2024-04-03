17b3fe989a723e66202b8ce35d1a7e9027e5026d
Hihiz <public@mail.ru>
2024-04-03 23:49:31 +0300

    Создать команду добавления устройства

---
+++ PublisherComplexWeb.CQRS.Application/Devices/Commands/CreateDevice/CreateDeviceCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Devices.Commands.CreateDevice
+{
+    public class CreateDeviceCommandHandler : IRequestHandler<CreateDeviceCommand>
+    {
+        private readonly IBaseRepository<Device> _repository;
+        private readonly IMapper _mapper;
+
+        public CreateDeviceCommandHandler(IBaseRepository<Device> repository, IMapper mapper) =>
+           (_repository, _mapper) = (repository, mapper);
+
+        public async Task<CreateDeviceDto> Handle(CreateDeviceCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
