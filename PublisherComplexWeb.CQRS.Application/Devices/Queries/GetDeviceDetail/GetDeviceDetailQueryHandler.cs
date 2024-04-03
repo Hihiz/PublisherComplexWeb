f78b10a2686fc92e7c6cc4c65a3dc6b76bdcae94
Hihiz <public@mail.ru>
2024-04-03 23:40:23 +0300

    Создать запрос получения устройства по id

---
+++ PublisherComplexWeb.CQRS.Application/Devices/Queries/GetDeviceDetail/GetDeviceDetailQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Devices.Queries.GetDeviceDetail
+{
+    public class GetDeviceDetailQueryHandler : IRequestHandler<GetDeviceDetailQuery, DeviceDto>
+    {
+        private readonly IBaseRepository<Device> _repository;
+        private readonly IMapper _mapper;
+
+        public GetDeviceDetailQueryHandler(IBaseRepository<Device> repository, IMapper mapper) =>
+            (_repository, _mapper) = (repository, mapper);
+
+        public async Task<DeviceDto> Handle(GetDeviceDetailQuery, CancellationToken cancellationToken)
+        {
+            try
+            {
+            
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
