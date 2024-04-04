9ca6ac4f90a43732ee6a17307c34a65ddd1d90c7
Hihiz <public@mail.ru>
2024-04-04 23:46:52 +0300

    Создать команду удаления формата

---
+++ PublisherComplexWeb.CQRS.Application/Formats/Commands/DeleteFormat/DeleteFormatCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Formats.Commands.DeleteFormat
+{
+    public class DeleteFormatCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+
+        public DeleteFormatCommand(int id) => Id = id;
+    }
+}
