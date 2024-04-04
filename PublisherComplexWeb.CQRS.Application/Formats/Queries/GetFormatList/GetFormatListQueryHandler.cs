f6c3bab5b0944447b9732974b12500d24116f51b
Hihiz <public@mail.ru>
2024-04-04 23:54:07 +0300

    Создать запрос получения списка форматов

---
+++ PublisherComplexWeb.CQRS.Application/Formats/Queries/GetFormatList/GetFormatListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Formats.Queries.GetFormatList
+{
+    public class GetFormatListQueryHandler : IRequestHandler<GetFormatListQuery, List<Format>>
+    {
+        private readonly IMapper _mapper;
+
+        public GetFormatListQueryHandler(IBaseRepository<Format> repository, IMapper mapper) =>
+            (_repository, _mapper) = (repository, mapper);
+
+        public async Task<List<Format>> Handle(GetFormatListQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
