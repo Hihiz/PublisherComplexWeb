67e069f04a27f0cd8c4d2c9af77bd8b44558851e
Hihiz <public@mail.ru>
2024-04-04 23:49:02 +0300

    Создать команду редактирования формата

---
+++ PublisherComplexWeb.CQRS.Application/Formats/Commands/UpdateFormat/UpdateFormatCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Formats.Commands.UpdateFormat
+{
+    public class UpdateFormatCommandHandler : IRequestHandler<UpdateFormatCommand, FormatDto>
+    {
+        private readonly IBaseRepository<Format> _repository;
+        private readonly IValidatorDto<Format> _validator;
+
+        public async Task<FormatDto> Handle(UpdateFormatCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+              
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
