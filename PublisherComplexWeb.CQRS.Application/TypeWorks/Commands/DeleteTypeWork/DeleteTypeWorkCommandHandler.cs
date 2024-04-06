feacb88098211a9d72cc01d50e269b4054dccf7e
Hihiz <public@mail.ru>
2024-04-07 01:10:40 +0300

    Создать команду удаления типа работ

---
+++ PublisherComplexWeb.CQRS.Application/TypeWorks/Commands/DeleteTypeWork/DeleteTypeWorkCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.TypeWorks.Commands.DeleteFormat
+{
+    public class DeleteTypeWorkCommandHandler : IRequestHandler<DeleteTypeWorkCommand, int>
+    {
+        private readonly IBaseRepository<TypeWork> _repository;
+
+        public DeleteTypeWorkCommandHandler(IBaseRepository<TypeWork> repository) =>
+            _repository = repository;
+
+        public async Task<int> Handle(DeleteTypeWorkCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+               
+            }
+        }
+    }
+}
