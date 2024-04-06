a2b57bd08105c46d7840129f015981e715173e08
Hihiz <public@mail.ru>
2024-04-07 01:58:00 +0300

    Создать команду удаления работы

---
+++ PublisherComplexWeb.CQRS.Application/Works/Commands/DeleteWork/DeleteWorkCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Works.Commands.DeleteWork
+{
+    public class DeleteWorkCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+
+        public DeleteWorkCommand(int id) => Id = id;
+    }
+}
---
+++ PublisherComplexWeb.CQRS.Application/Works/Commands/DeleteWork/DeleteWorkCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Works.Commands.DeleteWork
+{
+    public class DeleteWorkCommandHandler : IRequestHandler<DeleteWorkCommand, int>
+    {
+        private readonly IBaseRepository<Work> _repository;
+
+        public DeleteWorkCommandHandler(IBaseRepository<Work> repository) =>
+            _repository = repository;
+
+        public async Task<int> Handle(DeleteWorkCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+             
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
