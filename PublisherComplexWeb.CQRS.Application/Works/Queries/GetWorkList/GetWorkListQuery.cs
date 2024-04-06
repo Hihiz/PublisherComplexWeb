48d9481c5b5f27443f3fda5208d1409882b0c364
Hihiz <public@mail.ru>
2024-04-07 02:03:09 +0300

    Создать запрос получения списка работ 2

---
+++ PublisherComplexWeb.CQRS.Application/Works/Queries/GetWorkList/GetWorkListQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Works.Queries.GetWorkList
+{
+    public class GetWorkListQuery : IRequest<List<WorkDto>>
+    {
+        public int Id { get; set; }
+    }
+}
