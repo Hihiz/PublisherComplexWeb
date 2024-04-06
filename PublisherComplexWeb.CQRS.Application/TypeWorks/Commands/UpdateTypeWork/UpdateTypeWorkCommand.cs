aa8c652eea7279278d974e67d4c26bfb33556ff5
Hihiz <public@mail.ru>
2024-04-07 01:11:36 +0300

    Создать команду редактирования типа работ

---
+++ PublisherComplexWeb.CQRS.Application/TypeWorks/Commands/UpdateTypeWork/UpdateTypeWorkCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.TypeWorks.Commands.UpdateFormat
+{
+    public class UpdateTypeWorkCommand : IRequest<TypeWorkDto>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
