d4bd3b2eb631037c5386ed5273a0cd909c1ae5e2
Hihiz <public@mail.ru>
2024-04-07 02:09:58 +0300

    Создать команду редактирования материала

---
+++ PublisherComplexWeb.CQRS.Application/Materials/Commands/UpdateMaterial/UpdateMaterialCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Materials.Commands.UpdateMaterial
+{
+    public class UpdateMaterialCommandHandler : IRequestHandler<UpdateMaterialCommand, MaterialDto>
+    {
+        private readonly IBaseRepository<Material> _repository;
+        private readonly IMapper _mapper;
+        private readonly IValidatorDto<Material> _validator;
+
+        public UpdateMaterialCommandHandler(IBaseRepository<Material> repository, IMapper mapper, IValidatorDto<Material> validator) =>
+            (_repository, _mapper, _validator) = (repository, mapper, validator);
+
+        public async Task<MaterialDto> Handle(UpdateMaterialCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+              
+            }
+            catch (Exception ex)
+            {
+              
+            }
+        }
+    }
+}
