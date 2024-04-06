e5f9b20d4f0beeda4d9eb66b4ed2a05b2b199bdf
Hihiz <public@mail.ru>
2024-04-06 23:47:02 +0300

    Создать команду редактирования заказа

---
+++ PublisherComplexWeb.CQRS.Application/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand, OrderDto>
+    {
+        private readonly IBaseRepository<Order> _repository;
+        private readonly ICurrentUserService _currentUserService;
+
+        public UpdateOrderCommandHandler(IBaseRepository<Order> repository, ICurrentUserService currentUserService) =>
+            (_repository, _currentUserService) = (repository, currentUserService);
+
+        public async Task<OrderDto> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+              
+            }
+            catch (Exception ex)
+            {
+             
+            }
+        }
+    }
+}
