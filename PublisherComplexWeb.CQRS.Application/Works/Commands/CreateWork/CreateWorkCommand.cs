3787f41db59ca076fde74480f178fb25a039e3c9
Hihiz <public@mail.ru>
2024-04-07 01:56:50 +0300

    Создать команду добавления работы

---
+++ PublisherComplexWeb.CQRS.Application/Works/Commands/CreateWork/CreateWorkCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Works.Commands.CreateWork
+{
+    public class CreateWorkCommand : IRequest<CreateWorkDto>
+    {
+        public int OrderId { get; set; }
+        public int TypeWorkId { get; set; }
+        public int MaterialId { get; set; }
+    }
+}
---
+++ PublisherComplexWeb.CQRS.Application/Works/Commands/CreateWork/CreateWorkCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Works.Commands.CreateWork
+{
+    public class CreateWorkCommandHandler : IRequestHandler<CreateWorkCommand, CreateWorkDto>
+    {
+        private readonly IBaseRepository<Work> _repository;
+        private readonly IMapper _mapper;
+
+        public CreateWorkCommandHandler(IBaseRepository<Work> repository, IMapper mapper) =>
+            (_repository, _mapper) = (repository, mapper);
+
+        public async Task<CreateWorkDto> Handle(CreateWorkCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+ 
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
