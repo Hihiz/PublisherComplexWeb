06dd8952fa556390a8fa7dc22761705dcd677cf1
Hihiz <public@mail.ru>
2024-04-07 02:12:01 +0300

    Создать запрос получения материала по id

---
+++ PublisherComplexWeb.CQRS.Application/Materials/Queries/GetMaterialDetail/GetMaterialDetailQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.Materials.Queries.GetMaterialDetail
+{
+    public class GetMaterialDetailQuery : IRequest<MaterialDto>
+    {
+        public int Id { get; set; }
+
+        public GetMaterialDetailQuery(int id) => Id = id;
+    }
+}
