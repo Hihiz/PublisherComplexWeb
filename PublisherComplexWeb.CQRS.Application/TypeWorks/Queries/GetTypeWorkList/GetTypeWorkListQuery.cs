b06d453696cb44d5a17e50246b4c54f041695822
Hihiz <public@mail.ru>
2024-04-07 01:30:55 +0300

    Создать запрос получения списка типов работ

---
+++ PublisherComplexWeb.CQRS.Application/TypeWorks/Queries/GetTypeWorkList/GetTypeWorkListQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.TypeWorks.Queries.GetFormatList
+{
+    public class GetTypeWorkListQuery : IRequest<List<TypeWorkDto>>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ PublisherComplexWeb.CQRS.Application/TypeWorks/Queries/GetTypeWorkList/GetTypeWorkListQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.TypeWorks.Queries.GetFormatList
+{
+    public class GetTypeWorkListQueryHandler : IRequestHandler<GetTypeWorkListQuery, List<TypeWorkDto>>
+    {
+        private readonly IBaseRepository<TypeWork> _repository;
+        private readonly IMapper _mapper;
+
+        public GetTypeWorkListQueryHandler(IBaseRepository<TypeWork> repository, IMapper mapper) =>
+            (_repository, _mapper) = (repository, mapper);
+
+        public async Task<List<TypeWorkDto>> Handle(GetTypeWorkListQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
