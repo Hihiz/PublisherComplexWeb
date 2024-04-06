25ed1ceb7fa07c5050cd3f68ca588593e9df28c0
Hihiz <public@mail.ru>
2024-04-07 01:29:09 +0300

    Создать запрос получения типа работы по id

---
+++ PublisherComplexWeb.CQRS.Application/TypeWorks/Queries/GetTypeWorkDetail/GetTypeWorkDetailQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PublisherComplexWeb.CQRS.Application.TypeWorks.Queries.GetFormatDetail
+{
+    public class GetTypeWorkDetailQuery : IRequest<TypeWorkDto>
+    {
+        public int Id { get; set; }
+
+        public GetTypeWorkDetailQuery(int id) => Id = id;
+    }
+}
