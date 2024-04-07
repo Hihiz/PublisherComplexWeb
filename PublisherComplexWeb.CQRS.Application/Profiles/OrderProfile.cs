3b980baffe99330314fe47bad0b8493b55e72250
Hihiz <public@mail.ru>
2024-04-07 15:32:08 +0300

    Создать профиль автомапера для заказов

---
+++ PublisherComplexWeb.CQRS.Application/Profiles/OrderProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace PublisherComplexWeb.CQRS.Application.Profiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+            CreateMap<Order, CreateOrderCommand>().ReverseMap();
+
+        }
+    }
+}
