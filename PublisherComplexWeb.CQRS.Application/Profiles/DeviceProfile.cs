bca557b022993f37523d6beef4f05dbb183b22e7
Hihiz <public@mail.ru>
2024-04-07 15:30:18 +0300

    Создать профиль автомапера для устройств

---
+++ PublisherComplexWeb.CQRS.Application/Profiles/DeviceProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace PublisherComplexWeb.CQRS.Application.Profiles
+{
+    public class DeviceProfile : Profile
+    {
+        public DeviceProfile()
+        {
+            CreateMap<Device, CreateDeviceCommand>().ReverseMap();
+            CreateMap<Device, UpdateDeviceCommand>().ReverseMap();
+        }
+    }
+}
