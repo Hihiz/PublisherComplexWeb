b42d79b93a0b8c4da5b6b538b19ad5b16892f66f
Hihiz <public@mail.ru>
2024-04-15 23:57:13 +0300

    Добавить базовый сервис

---
+++ PublisherComplexWeb.Application/Interfaces/Services/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿namespace PublisherComplexWeb.Application.Interfaces.Services
+{
+    public interface IBaseService<T, TCreateDto, TUpdateDto>
+    {
+        Task<IBaseStatus<List<T>>> GetAll();
+        Task<IBaseStatus<T>> GetById(int id);
+        Task<IBaseStatus<T>> Create(TCreateDto dto);
+        Task<IBaseStatus<T>> Update(int id, TUpdateDto dto);
+        Task Delete(int id);
+    }
+}
