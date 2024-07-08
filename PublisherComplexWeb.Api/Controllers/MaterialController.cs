fa988f353d3fb82812e6c791748813fb4857901e
Hihiz <public@mail.ru>
2024-07-08 15:23:55 +0300

    Проверить получение данных

--- PublisherComplexWeb.Api/Controllers/MaterialController.cs
+++ PublisherComplexWeb.Api/Controllers/MaterialController.cs
@@ -1,9 +1,5 @@
-﻿using FluentValidation.Results;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using PublisherComplexWeb.Application.Dto.Material;
-using PublisherComplexWeb.Application.Interfaces;
-using PublisherComplexWeb.Application.Validations.FluentValidations.Material;
+﻿using PublisherComplexWeb.Application.Dto.Material;
+using PublisherComplexWeb.Application.Interfaces.Services;
 
 namespace PublisherComplexWeb.Api.Controllers
 {
@@ -11,58 +7,11 @@ namespace PublisherComplexWeb.Api.Controllers
     [ApiController]
     public class MaterialController : ControllerBase
     {
-        private readonly IMaterialService _materialService;
+        private readonly IBaseService<MaterialDto, CreateMaterialDto, UpdateMaterialDto> _materialService;
 
-        public MaterialController(IMaterialService materialService) => _materialService = materialService;
+        public MaterialController(IBaseService<MaterialDto, CreateMaterialDto, UpdateMaterialDto> materialService) => _materialService = materialService;
 
         [HttpGet]
         public async Task<ActionResult> GetMaterials() => Ok(await _materialService.GetAll());
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult> GetMaterial(int id) => Ok(await _materialService.GetById(id));
-
-        //[Authorize(Roles = "Member, Admin")]
-        [Authorize]
-        [HttpPost]
-        public async Task<ActionResult> CreateMaterial(CreateMaterialDto dto)
-        {
-            CreateMaterialValidator validators = new CreateMaterialValidator();
-
-            ValidationResult result = validators.Validate(dto);
-
-            if (!result.IsValid)
-            {
-                return BadRequest(string.Join('\n', result.Errors));
-            }
-
-            return Ok(await _materialService.CreateMaterial(dto));
-        }
-
-        //[Authorize(Roles = "Member, Admin")]
-        [Authorize]
-        [HttpPut("{id}")]
-        public async Task<ActionResult> UpdateMaterial(int id, UpdateMaterialDto dto)
-        {
-            UpdateMaterialValidator validators = new UpdateMaterialValidator();
-
-            ValidationResult result = validators.Validate(dto);
-
-            if (!result.IsValid)
-            {
-                return BadRequest(string.Join('\n', result.Errors));
-            }
-
-            return Ok(await _materialService.UpdateMaterial(id, dto));
-        }
-
-        //[Authorize(Roles = "Admin")]
-        [Authorize]
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteMaterial(int id)
-        {
-            await _materialService.DeleteMaterial(id);
-
-            return Ok(id);
-        }
     }
 }
