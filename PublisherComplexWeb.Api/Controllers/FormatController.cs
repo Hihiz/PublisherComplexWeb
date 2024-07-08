bb4165a1d1311146fb00537b9aa6e7e9085cf45b
Hihiz <public@mail.ru>
2024-07-08 15:22:48 +0300

    Проверить получение данных

--- PublisherComplexWeb.Api/Controllers/FormatController.cs
+++ PublisherComplexWeb.Api/Controllers/FormatController.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using PublisherComplexWeb.Application.Dto.Format;
-using PublisherComplexWeb.Application.Interfaces;
-using PublisherComplexWeb.Application.Validations.FluentValidations.Format;
-
-using ValidationResult = FluentValidation.Results.ValidationResult;
+﻿using PublisherComplexWeb.Application.Dto.Format;
 
 namespace PublisherComplexWeb.Api.Controllers
 {
@@ -12,58 +6,11 @@ namespace PublisherComplexWeb.Api.Controllers
     [ApiController]
     public class FormatController : ControllerBase
     {
-        private readonly IFormatService _formatService;
+        private readonly IBaseService<FormatDto, CreateFormatDto, UpdateFormatDto> _formatService;
 
-        public FormatController(IFormatService formatService) => _formatService = formatService;
+        public FormatController(IBaseService<FormatDto, CreateFormatDto, UpdateFormatDto> formatService) => _formatService = formatService;
 
         [HttpGet]
         public async Task<ActionResult> GetFormats() => Ok(await _formatService.GetAll());
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult> GetFormat(int id) => Ok(await _formatService.GetById(id));
-
-        //[Authorize(Roles = "Member, Admin")]
-        [Authorize]
-        [HttpPost]
-        public async Task<ActionResult> CreateFormat(CreateFormatDto dto)
-        {
-            CreateFormatValidator validators = new CreateFormatValidator();
-
-            ValidationResult result = validators.Validate(dto);
-
-            if (!result.IsValid)
-            {
-                return BadRequest(string.Join('\n', result.Errors));
-            }
-
-            return Ok(await _formatService.CreateFormat(dto));
-        }
-
-        //[Authorize(Roles = "Member, Admin")]
-        [Authorize]
-        [HttpPut("{id}")]
-        public async Task<ActionResult> UpdateFormat(int id, UpdateFormatDto dto)
-        {
-            UpdateFormatValidator validators = new UpdateFormatValidator();
-
-            ValidationResult result = validators.Validate(dto);
-
-            if (!result.IsValid)
-            {
-                return BadRequest(string.Join('\n', result.Errors));
-            }
-
-            return Ok(await _formatService.UpdateFormat(id, dto));
-        }
-
-        //[Authorize(Roles = "Admin")]
-        [Authorize]
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteFormat(int id)
-        {
-            await _formatService.DeleteFormat(id);
-
-            return Ok(id);
-        }
     }
 }
