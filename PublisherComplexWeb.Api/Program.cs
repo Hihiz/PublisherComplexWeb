54ce37e5d1ac28ff97facd3bcd6c06d69a408114
Hihiz <public@mail.ru>
2024-07-11 22:29:07 +0300

    Зарегистрировать зависимость grpc

--- PublisherComplexWeb.Api/Program.cs
+++ PublisherComplexWeb.Api/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
+builder.Services.AddGrpc().AddJsonTranscoding();
+
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
