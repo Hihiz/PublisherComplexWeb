50128c0fa795a221b4af886a355e843c4447afaf
Hihiz <public@mail.ru>
2024-07-11 22:34:06 +0300

    Редактировать дто работ

--- PublisherComplexWeb.Application/Dto/Work/WorkDto.cs
+++ PublisherComplexWeb.Application/Dto/Work/WorkDto.cs
@@ -3,18 +3,6 @@
     public class WorkDto
     {
         public int Id { get; set; }
-
-        public int OrderId { get; set; }
-
-        public int TypeWorkId { get; set; }
-        public string TypeWorkTitle { get; set; }
-
-        public int MaterialId { get; set; }
-        public string MaterialTitle { get; set; }
-
-        public int FormatId { get; set; }
-        public string FormatTitle { get; set; }
-
         public string DoublePrint { get; set; }
         public int? Quantity { get; set; }
         public string StatusWork { get; set; }
