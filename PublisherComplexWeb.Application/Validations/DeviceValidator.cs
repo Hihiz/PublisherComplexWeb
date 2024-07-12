dd77468b2311dca5c6e3bddd1c50c5672c1c7387
Hihiz <public@mail.ru>
2024-07-12 23:22:56 +0300

    Изменить реализацию валидаторов

--- PublisherComplexWeb.Application/Validations/DeviceValidator.cs
+++ PublisherComplexWeb.Application/Validations/DeviceValidator.cs
@@ -1,34 +1,7 @@
-﻿using PublisherComplexWeb.Application.Interfaces;
-using PublisherComplexWeb.Application.Models;
-using PublisherComplexWeb.Domain.Entities;
-using PublisherComplexWeb.Domain.Enums;
-
-namespace PublisherComplexWeb.Application.Validations
+﻿namespace PublisherComplexWeb.Application.Validations
 {
-    public class DeviceValidator : IValidatorDto<Device>
+    public class DeviceValidator
     {
-        private readonly IBaseRepository<Device> _repository;
-
-        public DeviceValidator(IBaseRepository<Device> repository) => _repository = repository;
-
-        public async Task<IBaseStatus> Validate(Device entity, CancellationToken cancellationToken)
-        {
-            Device result = (await _repository.GetAll(cancellationToken)).FirstOrDefault(d => d.Title == entity.Title);
-
-            if (result != null)
-            {
-                return new StatusResponse()
-                {
-                    StatusCode = (int)ErrorCode.AlreadyExists,
-                    Message = ErrorCode.AlreadyExists.ToString()
-                };
-            }
-
-            return new StatusResponse()
-            {
-                StatusCode = (int)ErrorCode.OK,
-                Message = ErrorCode.OK.ToString()
-            };
-        }
+      
     }
 }
--- PublisherComplexWeb.Application/Validations/FormatValidator.cs
+++ PublisherComplexWeb.Application/Validations/FormatValidator.cs
@@ -1,34 +1,7 @@
-﻿using PublisherComplexWeb.Application.Interfaces;
-using PublisherComplexWeb.Application.Models;
-using PublisherComplexWeb.Domain.Entities;
-using PublisherComplexWeb.Domain.Enums;
-
-namespace PublisherComplexWeb.Application.Validations
+﻿namespace PublisherComplexWeb.Application.Validations
 {
-    public class FormatValidator : IValidatorDto<Format>
+    public class FormatValidator
     {
-        private readonly IBaseRepository<Format> _repository;
-
-        public FormatValidator(IBaseRepository<Format> repository) => _repository = repository;
-
-        public async Task<IBaseStatus> Validate(Format entity)
-        {
-            Format result = (await _repository.GetAll()).FirstOrDefault(f => f.Title == entity.Title);
-
-            if (result != null)
-            {
-                return new StatusResponse()
-                {
-                    StatusCode = (int)ErrorCode.AlreadyExists,
-                    Message = ErrorCode.AlreadyExists.ToString()
-                };
-            }
-
-            return new StatusResponse()
-            {
-                StatusCode = (int)ErrorCode.OK,
-                Message = ErrorCode.OK.ToString()
-            };
-        }
+       
     }
 }
