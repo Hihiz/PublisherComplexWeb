8fa31b8555b97c75a41f51a11a3ce79996948c86
Hihiz <public@mail.ru>
2024-07-14 01:40:10 +0300

    Изменить ошибку с using

--- PublisherComplexWeb.CQRS.Application/Devices/Commands/CreateDevice/CreateDeviceCommand.cs
+++ PublisherComplexWeb.CQRS.Application/Devices/Commands/CreateDevice/CreateDeviceCommand.cs
@@ -1,4 +1,7 @@
-﻿namespace PublisherComplexWeb.CQRS.Application.Devices.Commands.CreateDevice
+﻿using MediatR;
+using PublisherComplexWeb.Application.Dto.Device;
+
+namespace PublisherComplexWeb.CQRS.Application.Devices.Commands.CreateDevice
 {
     public class CreateDeviceCommand : IRequest<CreateDeviceDto>
     {
