fcbf8c0abd23d4c751bffa2163acd6dd5deeb1b7
Hihiz <public@mail.ru>
2024-07-14 01:38:00 +0300

    Добавить метод действия получения форматов

--- PublisherComplexWeb.CQRS.Api/Controllers/FormatController.cs
+++ PublisherComplexWeb.CQRS.Api/Controllers/FormatController.cs
@@ -1,6 +1,23 @@
-﻿namespace PublisherComplexWeb.CQRS.Api.Controllers
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PublisherComplexWeb.CQRS.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class FormatController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FormatController(IMediator mediator) => _mediator = mediator;
+
+        [HttpGet]
+        public async Task<ActionResult> GetFormats()
+        {
+            GetFormatListQuery query = new GetFormatListQuery();
+            List<FormatDto> formmats = await _mediator.Send(query);
+
+            return Ok(formmats);
+        }
+    }
 }
