b0f5e11de7322ab400bfd3d3fd5e81c595555273
Hihiz <public@mail.ru>
2024-07-16 23:18:51 +0300

    Подготовить оснвной класс

--- PublisherComplexWeb.Infrastructure/Identity/ApplicationUser.cs
+++ PublisherComplexWeb.Infrastructure/Identity/ApplicationUser.cs
@@ -4,7 +4,5 @@ namespace PublisherComplexWeb.Infrastructure.Identity
 {
     public class ApplicationUser : IdentityUser
     {      
-        public string? RefreshToken { get; set; }
-        public DateTime RefreshTokenExpiryTime { get; set; }
     }
 }
