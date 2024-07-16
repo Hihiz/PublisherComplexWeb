a41fda70c4246319b47c28094d64e61b7ffed94e
Hihiz <public@mail.ru>
2024-07-16 23:16:41 +0300

    В классе identity user изменить тип ключа

--- PublisherComplexWeb.Infrastructure/Identity/ApplicationUser.cs
+++ PublisherComplexWeb.Infrastructure/Identity/ApplicationUser.cs
@@ -2,13 +2,8 @@
 
 namespace PublisherComplexWeb.Infrastructure.Identity
 {
-    public class ApplicationUser : IdentityUser<long>
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
-        public DateTime RegisteredIn { get; set; } = DateTime.UtcNow;
-
+    public class ApplicationUser : IdentityUser
+    {      
         public string? RefreshToken { get; set; }
         public DateTime RefreshTokenExpiryTime { get; set; }
     }
