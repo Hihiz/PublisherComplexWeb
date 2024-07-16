b64040b3f97a50b8e0bc585b5d92b603f2e3ba0a
Hihiz <public@mail.ru>
2024-07-16 23:05:34 +0300

    Обьединить модель

--- PublisherComplexWeb.Application/Models/StatusResponse.cs
+++ PublisherComplexWeb.Application/Models/StatusResponse.cs
@@ -2,21 +2,10 @@
 
 namespace PublisherComplexWeb.Application.Models
 {
-    public class StatusResponse : IBaseStatus
+    public class StatusResponse<T, TList> : IBaseStatus
     {
-        public int StatusCode { get; set; }
         public string Message { get; set; }
-    }
-
-    public class StatusResponse<T> : IBaseStatus<T>
-    {
-        public int StatusCode { get; set; }
         public T Data { get; set; }
-    }
-
-    public class StatusResponseError<T> : IBaseStatus<T>
-    {
-        public int StatusCode { get; set; }
-        public string Message { get; set; }
+        public List<TList> Datas { get; set; }
     }
 }
