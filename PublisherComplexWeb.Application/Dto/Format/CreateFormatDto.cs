9e11eaa54074cd0769e6c5538348d800b077b876
Hihiz <public@mail.ru>
2024-07-16 23:02:37 +0300

    Перенести классы дто форматов

--- PublisherComplexWeb.Application/Dto/Format/CreateFormatDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PublisherComplexWeb.Application.Dto.Format
-{
-    public class CreateFormatDto
-    {
-        public string Title { get; set; }
-    }
-}
--- PublisherComplexWeb.Application/Dto/Format/UpdateFormatDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PublisherComplexWeb.Application.Dto.Format
-{
-    public class UpdateFormatDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-    }
-}
