d6ecd3f1062ce79e360f09311b0fdc7839dd5307
Hihiz <public@mail.ru>
2024-07-16 23:02:25 +0300

    Перенести классы дто устройств

--- PublisherComplexWeb.Application/Dto/Device/CreateDeviceDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PublisherComplexWeb.Application.Dto.Device
-{
-    public class CreateDeviceDto
-    {
-        public string Title { get; set; }
-    }
-}
--- PublisherComplexWeb.Application/Dto/Device/UpdateDeviceDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PublisherComplexWeb.Application.Dto.Device
-{
-    public class UpdateDeviceDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-    }
-}
